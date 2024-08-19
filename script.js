8bc047feb0b18e162480416616c0883b710f459c
aisa <aisabihorac965@gmail.com>
2024-08-19 12:48:14 +0200

    Zadatak 1  - 3 bod

---
+++ script.js
@@ -0,0 +1,5 @@
+function IspisiText(){
+    document.getElementById("naslov").innerHTML="Nakon tigra, lav je druga mačka po veličiini, pa to znači da je najveća kopnena zvijer Afrike. Lav je od njuške do početka repa dugačak oko 180 cm, u ramenima doseže visinu od 120 cm a rep mu je dug 1 m. " ;
+}
+let button=document.getElementById("click-me");
+button.addEventListener("click",IspisiText);
